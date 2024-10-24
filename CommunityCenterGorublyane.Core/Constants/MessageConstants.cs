8b28bb3192e5cf897d27c0853212cdc24b9c1f12
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-10-24 23:22:03 +0300

    Message constants added

---
+++ CommunityCenterGorublyane.Core/Constants/MessageConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace CommunityCenterGorublyane.Core.Constants
+{
+    public static class MessageConstants
+    {
+        public const string RequiredMessage = "Полето '{0}' е задължително";
+
+        public const string LengthMessage = "Полето '{0}' трябва да е между {2} и {1} символа";
+ 
+    }
+}
