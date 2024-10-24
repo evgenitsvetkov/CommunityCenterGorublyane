75de51653e06bda4c23c80a07d999a599f1db6ab
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2024-10-24 16:47:11 +0300

    Repository added

---
+++ CommunityCenterGorublyane.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace CommunityCenterGorublyane.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+    }
+}
---
+++ CommunityCenterGorublyane.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CommunityCenterGorublyane.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+
+        public Repository(CommunityCenterDbContext _context)
+        {
+            context = _context;
+        }
+
+        private DbSet<T> Dbset<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return Dbset<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return Dbset<T>().AsNoTracking();   
+        }
+    }
+}
