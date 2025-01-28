cf1573df1e7e474a6169bb13fe3bc489d8faf7f6
Evgeni Tsvetkov <evgenicvetkov123@gmail.com>
2025-01-28 16:50:02 +0200

    Comment Component Added

---
+++ CommunityCenterGorublyane/Components/CommentComponent.cs
@@ -0,0 +1,23 @@
+﻿using CommunityCenterGorublyane.Core.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityCenterGorublyane.Components
+{
+    public class CommentComponent : ViewComponent
+    {
+        private readonly ICommentService commentService;
+
+        public CommentComponent(ICommentService _commentService)
+        {
+            commentService = _commentService;
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int newsId)
+        {
+            var comments = await commentService.AllCommentsAsync(newsId);
+
+            return await Task.FromResult<IViewComponentResult>(View(comments));
+        }
+    }
+}
